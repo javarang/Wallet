1684f679898b4eb8d5ce9aeba4191ac12d84dd03
javarang <70618991+javarang@users.noreply.github.com>
2022-10-06 16:36:30 +0900

    commit

---
+++ coupon/src/main/java/wallet/domain/Coupon.java
@@ -0,0 +1,57 @@
+package wallet.domain;
+
+import wallet.domain.CouponPurchased;
+import wallet.CouponApplication;
+import javax.persistence.*;
+import java.util.List;
+import lombok.Data;
+import java.util.Date;
+
+@Entity
+@Table(name="Coupon_table")
+@Data
+
+public class Coupon  {
+
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    
+    
+    
+    
+    
+    private Long id;
+    
+    
+    
+    
+    
+    private String name;
+    
+    
+    
+    
+    
+    private Long price;
+
+    @PostPersist
+    public void onPostPersist(){
+
+
+        CouponPurchased couponPurchased = new CouponPurchased(this);
+        couponPurchased.publishAfterCommit();
+
+    }
+
+    public static CouponRepository repository(){
+        CouponRepository couponRepository = CouponApplication.applicationContext.getBean(CouponRepository.class);
+        return couponRepository;
+    }
+
+
+
+
+
+
+}
